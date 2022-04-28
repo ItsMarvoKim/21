8306b0fc35cc6dc9387f547347215b384b3101d5
ItsMarvoKim <marvin.kithinji@student.moringaschool.com>
2022-04-28 07:09:50 +0300

    updated routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SearchPageComponent } from './components/search-page/search-page.component';
 
+
 const routes: Routes = [
   {
     path: '',
@@ -14,4 +15,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
